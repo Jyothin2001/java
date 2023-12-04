5c3168be3b3756ac3d5aa63d2ada2e8db1b0aa18
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-04 14:11:20 +0530

    primitive dt

---
+++ Vari.java
@@ -0,0 +1,265 @@
+public class Vari
+{
+public static void main(String []args)
+{
+	//float =4 byte(32 bits)= 6-7 total digits
+	
+float audioSeed = 1.5f;
+System.out.println(audioSeed);
+
+float instaFollowers = 1.5f;
+System.out.println(instaFollowers);
+
+instaFollowers = 1.2f;
+System.out.println(instaFollowers);
+		
+	
+float piValue=3.141f;
+System.out.println(piValue);
+
+float sgpa=6.4f;
+System.out.println(sgpa);
+
+sgpa=7.42f;//re-assingin
+System.out.println(sgpa);
+
+float fmRadioChannelNumber=98.3f;
+System.out.println(fmRadioChannelNumber);
+
+fmRadioChannelNumber=94.3f;//re-assingin
+System.out.println(fmRadioChannelNumber);
+
+float studentMarks=75.5f;
+System.out.println(studentMarks);
+
+studentMarks=99.5f;//re-assingin
+System.out.println(studentMarks);
+
+
+float exponentialConstant=2.178f;
+System.out.println(exponentialConstant);
+
+float cgpa=9.42f;
+System.out.println(cgpa);
+
+cgpa=7.42f;//re-assingin
+System.out.println(cgpa);
+
+float bankBalance=1000.50f;
+System.out.println(bankBalance);
+
+bankBalance=6000.50f;//re-assingin
+System.out.println(bankBalance);
+
+float studentPercentage=80.2f;
+System.out.println(studentPercentage);
+
+studentPercentage=98.5f;//re-assingin
+System.out.println(studentPercentage);
+
+float phoneBatteryPercentage=80f;
+System.out.println(phoneBatteryPercentage);
+
+phoneBatteryPercentage=90f;//re-assingin
+System.out.println(phoneBatteryPercentage);
+
+float kilometer=4.5f;
+System.out.println(kilometer);
+
+kilometer=7.5f;//reassingin
+System.out.println(kilometer);
+
+float weightOfAPerson=65.5f;
+System.out.println(weightOfAPerson);
+
+weightOfAPerson=45.5f;//reassingin
+System.out.println(weightOfAPerson);
+
+float height=6.5f;
+System.out.println(height);
+
+height=5.5f;//reassingin
+System.out.println(height);
+
+float circumference=6.25f;
+System.out.println(circumference);
+
+float mobileData=1.5f;
+System.out.println(mobileData);
+
+float radiusOfEarth=6.37f;
+System.out.println(radiusOfEarth);
+
+float phoneScreenTime=4.31f;
+System.out.println(phoneScreenTime);
+
+
+
+//double =8byte
+
+double faradaysConstant=9.648533289d;
+System.out.println(faradaysConstant);
+
+double CircumferenceOfCircle=6.283185307d;
+System.out.println(CircumferenceOfCircle);
+
+double speedOfLight=2.99792458d;
+System.out.println(speedOfLight);
+
+double electronRestMass=9.10938356d;
+System.out.println(electronRestMass);
+
+double redbergConstant=1.0973731568505d;
+System.out.println(redbergConstant);
+
+double protonRestMass=1.672621898d;
+System.out.println(protonRestMass);
+
+double chargeOnElectron=1.6021766208d;
+System.out.println(chargeOnElectron);
+
+double neutronRestMass=1.674927471d;
+System.out.println(neutronRestMass);
+
+double boltzmann=1.38064852d;
+System.out.println(boltzmann);
+
+double plancksConstan=6.626070040d;
+System.out.println(plancksConstan);
+
+double gravitationConstant=6.6743014d;
+System.out.println(gravitationConstant);
+
+double fineStructureConstant=7.2973525664d;
+System.out.println(fineStructureConstant);
+
+//char stores single value = 2byte
+
+char meters = 'm';
+System.out.println(meters);
+
+char seconds = 's';
+System.out.println(seconds);
+	
+char  volts = 'v';
+System.out.println(volts);
+
+char grade='A';
+System.out.println(grade);
+
+grade='B';//re-assingin
+System.out.println(grade);
+
+char result='P';
+System.out.println(result);
+
+result='F';//reassingin
+System.out.println(result);
+
+char gender='M';
+System.out.println(gender);
+
+gender='F';//reassingin
+System.out.println(gender);
+
+char nameInitial='R';
+System.out.println(nameInitial);
+
+nameInitial='y';//reassingin
+System.out.println(nameInitial);
+
+char meter='M';
+System.out.println(meter);
+
+char length='L';
+System.out.println(length);
+
+char speedOfLightSymbol='c';
+System.out.println(speedOfLightSymbol);
+
+char faradayConstantSymbol='f';
+System.out.println(faradayConstantSymbol);
+
+char electronsymbol='e';
+System.out.println(electronsymbol);
+
+char flanckConstant='h';
+System.out.println(flanckConstant);
+
+
+char radius='R';
+System.out.println(radius);
+ 
+char constantOfGravitationSymbol='G';
+System.out.println(constantOfGravitationSymbol);
+ 
+
+char multiplication='*';
+System.out.println(multiplication);
+
+multiplication='.';
+System.out.println(multiplication);
+
+char division='/';
+System.out.println();
+
+char plus='+';
+System.out.println(plus);
+
+
+// Boolean =true or false=1bit
+
+boolean sharksAreMammals=false;
+System.out.println(sharksAreMammals);
+
+boolean batsAreBlind=false;
+System.out.println(batsAreBlind);
+
+boolean southAfricaHasOneCapital=true;
+System.out.println(southAfricaHasOneCapital);
+
+boolean herbivoresAreAnimalEaters=false;
+System.out.println(herbivoresAreAnimalEaters);
+
+boolean octopusHasSevenHeart =false;
+System.out.println(octopusHasSevenHeart);
+
+boolean infantsHasMoreBonesThanAdults=true;
+System.out.println(infantsHasMoreBonesThanAdults);
+
+boolean humanBodyHasFourLungs=false;
+System.out.println(humanBodyHasFourLungs);
+
+boolean humanBrainIsMadeOfMuscle=false;
+System.out.println(humanBrainIsMadeOfMuscle);
+
+boolean humanBodyIsAboutSixtyPercentageWater=true;
+System.out.println(humanBodyIsAboutSixtyPercentageWater);
+
+boolean commonBloodTypeIsONegative=true;
+System.out.println(commonBloodTypeIsONegative);
+
+boolean toyStoryWasPixarsFirstFilm=true;
+System.out.println(toyStoryWasPixarsFirstFilm);
+
+boolean cheesecakeComesFromItaly=false;
+System.out.println(cheesecakeComesFromItaly);
+
+boolean astronautHasPlayedGolfOnTheMoon=true;
+System.out.println(astronautHasPlayedGolfOnTheMoon);
+
+boolean fortuneCookiesWereInventedInChina=false;
+System.out.println(fortuneCookiesWereInventedInChina);
+
+boolean vaticanCityIsTheSmallestCityInTheWorld=true;
+System.out.println(vaticanCityIsTheSmallestCityInTheWorld);
+
+boolean hummingBirdIsTheWorldsSmallestBird=true;
+System.out.println(hummingBirdIsTheWorldsSmallestBird);
+
+boolean octopusHasThreeHeart =true;
+System.out.println(octopusHasThreeHeart);
+
+}
+}
+
