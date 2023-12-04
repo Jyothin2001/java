f51ffd1f65d16d7dfbd40eacacab7a242ce40e46
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-04 14:06:56 +0530

    datatypes

---
+++ Variables.java
@@ -0,0 +1,141 @@
+public class Variables
+{
+	public static void main(String args[])
+	{
+		//byte = 1byte = -128 to 127
+		
+	 byte averageIqOfAPerson = 90; 
+	 System.out.println(averageIqOfAPerson);
+	
+	 byte VoterAge = 18;
+	 System.out.println(VoterAge);
+	 
+	 VoterAge=21;
+	 System.out.println(VoterAge);
+	
+	
+	 byte currentLifeExpectancyForIndia = 70;
+	 System.out.println(currentLifeExpectancyForIndia );
+	 
+	 byte statesInIndia=28;
+	 System.out.println(statesInIndia);
+	 
+	 byte monthsInYear =12;
+	 System.out.println(monthsInYear );
+	 
+	 byte heartBeatsForMinute = 60;
+	 System.out.println(heartBeatsForMinute);
+	 
+	 heartBeatsForMinute = 64;
+	 System.out.println(heartBeatsForMinute);
+	 
+	 //short = 2byte = -32,768 to +32,767
+	 
+	 short numberOfNationalHighwaysInIndia=599;
+	 System.out.println(numberOfNationalHighwaysInIndia);
+	 
+	 short tvChannelsInIndia =900;
+	 System.out.println(tvChannelsInIndia);
+	 
+	 short netflixPrice = 499;
+	 System.out.println(netflixPrice);
+	 
+	 netflixPrice = 600;
+	 System.out.println(netflixPrice);
+	 
+	 short newsChannelsInIndia = 392;
+	 System.out.println(newsChannelsInIndia);
+	 
+	 newsChannelsInIndia = 390;
+	 System.out.println(newsChannelsInIndia);
+	 
+	 short countriesInTheWorld = 195;
+	 System.out.println(countriesInTheWorld );
+	 
+	 short numberOfResortsINIndia = 28774;
+	  System.out.println(numberOfResortsINIndia);
+	  
+	  numberOfResortsINIndia = 28000;
+	  System.out.println(numberOfResortsINIndia);
+	 
+	 // int = 4byte = -214,74,83,648 to +2147483648
+	 
+	 int totalTemplesInIndia = 2000000;
+	  System.out.println(totalTemplesInIndia);
+	  
+	  int averageHumanHeadHas = 100000;
+	  System.out.println(averageHumanHeadHas);
+	  
+	  
+	   averageHumanHeadHas = 110000;
+	  System.out.println(averageHumanHeadHas);
+	  
+	  int privateHospitalsInIndia = 43486;
+	  System.out.println(privateHospitalsInIndia);
+	  
+	    privateHospitalsInIndia = 40000;
+	  System.out.println(privateHospitalsInIndia);
+	  
+	  int malePopulationInIndia = 649400000;
+	  System.out.println(malePopulationInIndia);
+	  
+	    malePopulationInIndia = 649500000;
+	  System.out.println(malePopulationInIndia);
+	  
+	   int femalePopulationInIndia = 604700000;
+	  System.out.println(femalePopulationInIndia);
+	  
+	  
+	   femalePopulationInIndia = 614700000;
+	  System.out.println(femalePopulationInIndia);
+	  
+	  
+	  
+	  //long = 8byte= -9223372036854775808 to +9223372036854775808
+	  
+
+	 long elonmuskNetworth = 2000000000000l;
+	 System.out.println(elonmuskNetworth);
+	 
+	  elonmuskNetworth = 2100000000000l;// reassigning
+	 System.out.println(elonmuskNetworth);
+	 
+	 
+	 long accountNumber=98654325894l;
+	 System.out.println(accountNumber);
+	 
+	 accountNumber=18654325894l;
+	 System.out.println(accountNumber);
+	 
+	 long populationOfIndia=140000000000l;
+	 System.out.println(populationOfIndia);
+	 
+	 populationOfIndia=146000000000l;
+	 System.out.println(populationOfIndia);
+	 
+	 long numberOfStarsInAGalaxy=100000000000l;
+	 System.out.println(numberOfStarsInAGalaxy);
+	 
+	 numberOfStarsInAGalaxy=190000000000l;
+	 System.out.println(numberOfStarsInAGalaxy);
+	 
+	 long phoneNumber=9875443579l;
+	 System.out.println(phoneNumber);
+	 
+	 phoneNumber=8975443579l;
+	 System.out.println(phoneNumber);
+	 
+	 
+	  
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	}
+}
