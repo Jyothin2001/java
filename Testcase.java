10598676b1777b11e77aba71add3bb1d9e00cf24
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-06 21:12:11 +0530

    methods

---
+++ Testcase.java
@@ -0,0 +1,45 @@
+public class Testcase
+{
+   public static void main(String[] args)
+   {
+       System.out.println("starting ");
+	   walk();
+	   jump();
+	   run();
+	   play();
+	   
+   }
+   static void walk()
+   {
+	   System.out.println("we walked here");
+   }
+   static void jump()
+   {
+	    System.out.println("the boy jumped out of bed");
+   }
+   static void run()
+   {
+	   System.out.println("run as fast as you can"); 
+   }
+   static void play()
+   {
+	   System.out.println("i like to play games");
+	    look();
+        stay();	   
+   }
+   static void look()
+   {
+	   System.out.println("look at my eyes"); 
+	  
+   }
+   static void stay()
+   {
+	   System.out.println("I stayed with my family and had fun"); 
+	   draw();
+   }
+   static void draw()
+   {
+	   System.out.println("i like to draw pictures"); 
+   }
+
+} 
