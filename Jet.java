512dd4c5ec950c0b36bceb41e3561d04b60470a8
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-14 22:46:06 +0530

    array problems

---
+++ Jet.java
@@ -0,0 +1,258 @@
+public class Jet
+{
+public static void main(String[] args)
+{
+	// one array //
+	
+  byte byteArray[]={(byte)19,(byte)126,(byte)31,(byte)22,(byte)33,(byte)67,(byte)86,(byte)44};
+  
+  // sum of an  array
+  
+       int sum=0;
+       for(int i=0; i< byteArray.length; i++)
+  
+	       sum=sum+byteArray[i];
+	       {
+              System.out.println( "sum = " + sum );
+           }
+	// even numberes of an array
+	
+		   for(int i=0; i< byteArray.length; i++)
+			   if(byteArray[i]%2==0)
+			   {
+				   System.out.println("even= "  +byteArray[i]);
+			   }
+		   
+		          
+	// odd numberes of an array
+	
+		   for(int i=0; i< byteArray.length; i++)
+			   if(byteArray[i]%2!=0)
+			   {
+				   System.out.println("odd= "  +byteArray[i]);
+			   }
+	// Reverse of an given array
+	
+	for(int i=byteArray.length-1; i>=0;i--)
+	{
+		System.out.println("Reverse= "+ +byteArray[i]);
+	}
+	
+	// printing from middle to right
+	
+	for(int i=(byteArray.length-1)/2; i<byteArray.length; i++)
+	{
+		System.out.println("right side= "+ +byteArray[i]);
+	}
+	
+	// printing from middle to left
+	
+	for(int i=(byteArray.length-1)/2; i>=0; i--)
+	{
+		System.out.println("left side= "+ +byteArray[i]);
+	}
+	
+	
+	// second array //
+	
+	
+	int intArray[]={1986,15423,31135,2267896,322443,6732345,8657897,44654,};
+  
+     // sum of an  array
+  
+       int sum1=0;
+       for(int i=0; i< intArray.length; i++)
+  
+	       sum1=sum1+intArray[i];
+	       {
+              System.out.println( "sum1 = " + sum1 );
+           }
+	// even numberes of an array
+	
+		   for(int i=0; i< intArray.length; i++)
+			   if(intArray[i]%2==0)
+			   {
+				   System.out.println("even= "  +intArray[i]);
+			   }
+		   
+		          
+	// odd numberes of an array
+	
+		   for(int i=0; i< intArray.length; i++)
+			   if(intArray[i]%2!=0)
+			   {
+				   System.out.println("odd= "  +intArray[i]);
+			   }
+	// Reverse of an given array
+	
+	for(int i=intArray.length-1; i>=0;i--)
+	{
+		System.out.println("Reverse= "+ +intArray[i]);
+	}
+	
+	// printing from middle to right
+	
+	for(int i=(intArray.length-1)/2; i<intArray.length; i++)
+	{
+		System.out.println("right side= "+ +intArray[i]);
+	}
+	
+	// printing from middle to left
+	
+	for(int i=(intArray.length-1)/2; i>=0; i--)
+	{
+		System.out.println("left side= "+ +intArray[i]);
+	}
+	
+	// third array //
+	
+  long[] longArray = {123456765432l,786956438907l,345678906432l,123452671234l,567843218907l,908756781234l,786565433456l,980765437890l};
+
+// sum of an  array
+  
+       long sum2=0;
+       for(int i=0; i< longArray.length; i++)
+  
+	       sum2=sum2+longArray[i];
+	       {
+              System.out.println( "sum2 = " + sum2 );
+           }
+	// even numberes of an array
+	
+		   for(int i=0; i< longArray.length; i++)
+			   if(longArray[i]%2==0)
+			   {
+				   System.out.println("even= "  +longArray[i]);
+			   }
+		   
+		          
+	// odd numberes of an array
+	
+		   for(int i=0; i< longArray.length; i++)
+			   if(longArray[i]%2!=0)
+			   {
+				   System.out.println("odd= "  +longArray[i]);
+			   }
+	// Reverse of an given array
+	
+	for(int i=longArray.length-1; i>=0;i--)
+	{
+		System.out.println("Reverse= "+ +longArray[i]);
+	}
+	
+	// printing from middle to right
+	
+	for(int i=(longArray.length-1)/2; i<longArray.length; i++)
+	{
+		System.out.println("right side= "+ +longArray[i]);
+	}
+	
+	// printing from middle to left
+	
+	for(int i=(longArray.length-1)/2; i>=0; i--)
+	{
+		System.out.println("left side= "+ +longArray[i]);
+	}
+	
+	// fourth array //
+	
+	float[] cgpa = { 22.0f,436.0f,698.93456f,843.045f,534.94567f,843.778f,85.625f,62.9322f};
+	
+	// sum of an  array
+  
+       float sum3=0;
+       for(int i=0; i< cgpa.length; i++)
+  
+	       sum3=sum3+cgpa[i];
+	       {
+              System.out.println( "sum3 = " + sum3 );
+           }
+	// even numberes of an array
+	
+		   for(int i=0; i< cgpa.length; i++)
+			   if(cgpa[i]%2==0)
+			   {
+				   System.out.println("even= "  +cgpa[i]);
+			   }
+		   
+		          
+	// odd numberes of an array
+	
+		   for(int i=0; i< cgpa.length; i++)
+			   if(cgpa[i]%2!=0)
+			   {
+				   System.out.println("odd= "  +cgpa[i]);
+			   }
+	// Reverse of an given array
+	
+	for(int i=cgpa.length-1; i>=0;i--)
+	{
+		System.out.println("Reverse= "+ +cgpa[i]);
+	}
+	
+	// printing from middle to right
+	
+	for(int i=(cgpa.length-1)/2; i<cgpa.length; i++)
+	{
+		System.out.println("right side= "+ +cgpa[i]);
+	}
+	
+	// printing from middle to left
+	
+	for(int i=(cgpa.length-1)/2; i>=0; i--)
+	{
+		System.out.println("left side= "+ +cgpa[i]);
+	}
+	
+	// fifth array //
+	
+	double[] doubleArray = {24864.d,21.344566d,36.45676543d,28.45678d,38.890754d,45.678654d,45.8765678d,89.765432d};
+	
+	// sum of an  array
+  
+       double sum4=0;
+       for(int i=0; i< doubleArray.length; i++)
+  
+	       sum4=sum4+doubleArray[i];
+	       {
+              System.out.println( "sum4 = " + sum4);
+           }
+	// even numberes of an array
+	
+		   for(int i=0; i< doubleArray.length; i++)
+			   if(doubleArray[i]%2==0)
+			   {
+				   System.out.println("even= "  +doubleArray[i]);
+			   }
+		   
+		          
+	// odd numberes of an array
+	
+		   for(int i=0; i< doubleArray.length; i++)
+			   if(doubleArray[i]%2!=0)
+			   {
+				   System.out.println("odd= "  +doubleArray[i]);
+			   }
+	// Reverse of an given array
+	
+	for(int i=doubleArray.length-1; i>=0;i--)
+	{
+		System.out.println("Reverse= "+ +doubleArray[i]);
+	}
+	
+	// printing from middle to right
+	
+	for(int i=(doubleArray.length-1)/2; i<doubleArray.length; i++)
+	{
+		System.out.println("right side= "+ +doubleArray[i]);
+	}
+	
+	// printing from middle to left
+	
+	for(int i=(doubleArray.length-1)/2; i>=0; i--)
+	{
+		System.out.println("left side= "+ +doubleArray[i]);
+	}
+
+}
+}
