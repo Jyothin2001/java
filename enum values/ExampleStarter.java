27271abdc5058914ca187e3611359d73f46234d7
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-27 14:42:03 +0530

    enum with constructor

--- enum values/ExampleStarter.java
+++ enum values/ExampleStarter.java
@@ -15,26 +15,11 @@ public static void main(String[] args)
 		System.out.println(obj.result.value);
 		System.out.println(obj.grade.value);
 		System.out.println(obj.days.value);
-		System.out.println(obj.light.value);*/
+		System.out.println(obj.light.value);
 		
 		BloodGroups ab = BloodGroups.A_POSITIVE; //way=store enum.its value to a variable
 	    System.out.println(ab);
-		 
-		 BloodGroups val1 = BloodGroups.B_POSITIVE; //way=store enum.its value to a variable
-	    System.out.println(val1);
 		
-		 if(val1.value == 0)            ////way1=store enum.its value to a variable then add .value to compare
-	   {
-		   System.out.println("one is greater than 1 ");
-		}
-		else
-		{
-			System.out.println("one is lesser than 1");
-		}
-		
-		
-		
-	   
 	    Semester abc = Semester.FIRST;
 	    System.out.println(abc); 
        
@@ -60,7 +45,7 @@ public static void main(String[] args)
 			System.out.println("the Given Rating value is 2  not equals to 2");
 		}
 		
-		if(obj.food.value=="veg") // way2=direct give .value to compare
+		if(obj.food.value=="VEG")    //way2=direct give .value to compare
 		{
 			System.out.println("yes! it is veg");
 		}
@@ -68,7 +53,14 @@ public static void main(String[] args)
 		{
 			System.out.println("no! it is not veg");
 		}
-		
+       if(ab.value == "A+ve")            ////way1=store enum.its value to a variable then add .value to compare
+	   {
+		   System.out.println("yes ");
+		}
+		else
+		{
+			System.out.println("no");
+		}
   
 }
 }
