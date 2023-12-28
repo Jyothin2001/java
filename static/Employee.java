387d9a9e2650abd06a6a3b28b65bab29c893009a
Jyothin2001 <jyothin.xworkz@gmail.com>
2023-12-28 08:35:26 +0530

    static example

---
+++ static/Employee.java
@@ -0,0 +1,14 @@
+public class Employee
+{
+static  String companyName="TCS";
+static String ceo="suma";
+static int companyLaptop=1;
+
+String employeeName="suri"; 
+String gender="Male";
+byte age=30;
+int salary=10000;
+
+
+
+}
